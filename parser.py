7b90e0b7a7d939bb2746e54f6a2467d260a32bc0
Nell Shamrell <nellshamrell@gmail.com>
2020-11-22 14:41:17 -0800

    more clean up
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- parser.py
+++ parser.py
@@ -15,16 +15,10 @@ import argparse
 import json
 import re
 import collections
-# import pyyaml
 import pprint
 pp = pprint.PrettyPrinter(indent=1)
 from operator import itemgetter
 from iteration_utilities import unique_everseen
-
-
-# Open Sites Mapping
-with open('./mappings/sites.json') as f:
-    sites = json.load(f)
 
 
 # Pre-compile regular expresions into an object
@@ -38,22 +32,6 @@ SiteCodeRegex       = re.compile(r'''(?<=\b)[a-zA-Z0-9]{4}(?=-)''')
 DevicesRegex        = re.compile(r'''(?<=-)[a-z]{2}(?<!-)''')
 UnitNameRegex       = re.compile(r'''(?<=:\s\s).*$''')
 HostNameRegex       = re.compile(r'''(?<=\b)[a-zA-Z0-9]{4}-[a-zA-Z0-9]{3}-[a-zA-Z0-9]{2}-[a-zA-Z0-9]{3}(?=\b)''')
-
-
-
-# class DynamicInventory(object):
-
-#     def __init__(self):
-#         self.inventory = {}
-#         self.read_cli_args()
-
-#         # Called with `--list`
-#         if self.args.list:
-#             self.inventory = self.dynamic_inventory()
-#         # Called with `--host [hostname]`.
-#         elif self.args.host:
-#             # Not implemented, sinced we return _meta info `--list`
-#             self.inventory = self.dynamic_inventory()
 
 def file_contents(path_to_file):
     output = []
@@ -75,41 +53,18 @@ def site_names_map(path_to_map_file):
         sites = json.load(f)
     return sites
 
-
-contents = file_contents('./tmp/hosts')
-#pp.pprint (content)
-
-site_names = site_names_map('./mappings/sites.json')
-pp.pprint (site_names)
-
 def replace_site_names(contents, site_names):
-#    site_expanded = []
-
+    
     for c in contents:
-#        print(c['site_name'])
         expanded_site_name = site_names[c['site_name']]
-#        print(expanded_site_name)
+
         # replace site name abbreviation with expanded site name
         c['site_name'] = expanded_site_name
-        print(c)
 
-    # iterate over content
-thing = replace_site_names(contents, site_names)
-#pp.pprint (thing)
+    return contents
 
-# Transform site names from mappings
-#groups = [li['site_name'] for li in content]
+contents = file_contents('./tmp/hosts')
+site_names = site_names_map('./mappings/sites.json')
 
-#def replace(list, dictionary):
-#    dict = {'groups':[sites.get(item, item) for item in list] }
-#    return [sites.get(item, item) for item in list]
-
-
-# Remove duplicates from groupings
-#children = (list(unique_everseen(replace(groups,sites))))
-
-#dict2 = {'groups':
-#    children
-#}
-
-#pp.pprint (dict2)
+output = replace_site_names(contents, site_names)
+pp.pprint(output)
